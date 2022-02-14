c6c1a9ef33a156c48f290aae6cd7bb7e4fff8744
aysunsevnc <97788573+aysunsevnc@users.noreply.github.com>
2022-02-14 23:00:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x =3;
+            int y =3;
+            y= y +2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y/=1;
+            Console.WriteLine(y);
+            x +=2;
+            Console.WriteLine(x);
+
+        
+
+
+        }
+    }
+}
